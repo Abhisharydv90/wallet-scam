fb3656cc8f6cedc12d84e397a0ea40cdb4d05b83
Abhisharydv90 <140367934+Abhisharydv90@users.noreply.github.com>
2026-01-19 21:29:39 +0530

    Create wallet.py

---
+++ api/wallet.py
@@ -0,0 +1,41 @@
+import os
+import hashlib
+import base58
+import eth_keys
+from solana import Keypair
+
+def generate_bitcoin_wallet():
+    private_key = os.urandom(32).hex()
+    public_key = hashlib.sha256(bytes.fromhex(private_key)).hexdigest()
+    address = base58.b58encode_check(
+        b'\x00' + bytes.fromhex(public_key[:64])
+    ).decode('utf-8')
+    
+    return {
+        'private_key': private_key,
+        'public_key': public_key,
+        'address': address
+    }
+
+def generate_ethereum_wallet():
+    private_key = os.urandom(32).hex()
+    public_key = hashlib.sha3_256(bytes.fromhex(private_key)).hexdigest()
+    address = '0x' + public_key[24:]
+    
+    return {
+        'private_key': private_key,
+        'public_key': public_key,
+        'address': address
+    }
+
+def generate_solana_wallet():
+    keypair = Keypair.generate()
+    private_key = keypair.secret_key.hex()
+    public_key = keypair.public_key.hex()
+    address = keypair.public_key.to_string()
+    
+    return {
+        'private_key': private_key,
+        'public_key': public_key,
+        'address': address
+    }
